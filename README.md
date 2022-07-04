5f99d162ff415562af8aa72713116af12fd621f2
Hemant Zope <hemant.zope@fokus.fraunhofer.de>
2022-07-04 20:40:10 +0200

    Update license

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Ansible module to parse ping response, uses python re to parse the response.
 play.yaml is an example play which shows how to use the library.
 
 ## How to use
-ansible-playbook play.yml
+`ansible-playbook play.yml`
--- library/parse_ping.py
+++ library/parse_ping.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Copyright: (c) 2022, Hemant Zope <hemantzope2008@gmail.com>
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 DOCUMENTATION = r"""
 ---
 module: parse_ping
